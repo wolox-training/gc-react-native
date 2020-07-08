1ec0ade89613b69c8f18276698d4fa0d5420d763
Gonzalo Cerbelli <gonzalo.cerbelli@wolox.com.ar>
2020-07-07 22:34:59 -0300

    add aliases to babel.config.js

--- wbooks/babel.config.js
+++ wbooks/babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset']
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@app': './src/app',
+          '@components': '.src/app/components/',
+          '@assets': './src/assets',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@interfaces': './src/interfaces',
+          '@redux': './src/redux',
+          '@services': './src/services',
+          '@utils': './src/utils'
+        }
+      }
+    ]
+  ]
 };
