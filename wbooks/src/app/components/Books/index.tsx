78a1c17b1ad78d86ac73d5f2d3ccd334e7f22ac4
Gonzalo Cerbelli <gonzalo.cerbelli@wolox.com.ar>
2020-07-16 10:12:16 -0300

    move render into component

--- wbooks/src/app/components/Books/index.tsx
+++ wbooks/src/app/components/Books/index.tsx
@@ -6,11 +6,10 @@ import { BOOKS_DATA } from '../../../constants/mocks';
 import { Book } from '../../interfaces/books';
 import { styles } from '../Books/styles';
 
-const renderItem = ({ item }: { item: Book }) => (
-  <BookCard title={item.title} author={item.author} img={item.img} />
-);
-
 const BooksList = () => {
+  const renderItem = ({ item }: { item: Book }) => (
+    <BookCard title={item.title} author={item.author} img={item.img} />
+  );
   const renderSeparator = () => <View style={styles.bookCardSeparator} />;
   return (
     <View style={styles.container}>
