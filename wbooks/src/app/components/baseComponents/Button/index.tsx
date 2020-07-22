b975d89a53eb7d0051ded02e5c587ea2c059491c
Gonzalo Cerbelli <gonzalo.cerbelli@wolox.com.ar>
2020-07-22 15:37:18 -0300

    refactor button styles

--- wbooks/src/app/components/baseComponents/Button/index.tsx
+++ wbooks/src/app/components/baseComponents/Button/index.tsx
@@ -4,20 +4,17 @@ import { Text, TouchableOpacity } from 'react-native';
 import { ButtonProps } from '../../../interfaces/button';
 import styles from './styles';
 
-const Button = ({ text, fill, onPress, disabled }: ButtonProps) => {
-  let buttonStyle = {};
-  if (disabled) {
-    buttonStyle = [styles.button, styles.disabledButton];
-  } else if (fill) {
-    buttonStyle = [styles.button, styles.fillButton];
-  } else {
-    buttonStyle = styles.button;
-  }
-  return (
-    <TouchableOpacity style={buttonStyle} disabled={disabled} onPress={onPress}>
-      <Text style={fill ? [styles.title, styles.fillButtonTitle] : styles.title}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
+const Button = ({ text, fill, onPress, disabled }: ButtonProps) => (
+  <TouchableOpacity
+    style={
+      disabled
+        ? [styles.button, fill && styles.fillButton, styles.disabledButton]
+        : [styles.button, fill && styles.fillButton]
+    }
+    disabled={disabled}
+    onPress={onPress}>
+    <Text style={fill ? [styles.title, styles.fillButtonTitle] : styles.title}>{text}</Text>
+  </TouchableOpacity>
+);
 
 export default Button;
