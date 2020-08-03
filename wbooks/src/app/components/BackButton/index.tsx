b8b8febad25479b0176148e7476407896d221f08
Gonzalo Cerbelli <gonzalo.cerbelli@wolox.com.ar>
2020-08-03 12:55:51 -0300

    use navigationStackProp

--- wbooks/src/app/components/BackButton/index.tsx
+++ wbooks/src/app/components/BackButton/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
+import { NavigationStackProp } from 'react-navigation-stack';
 
 import leftArrow from './assets/leftArrow.png';
 import styles from './styles';
 
 interface Props {
-  navigation: any;
+  navigation: NavigationStackProp;
 }
 
-const BackButton = ({ navigation }: Props) => (
-  <TouchableOpacity onPress={navigation.goBack}>
-    <Image style={styles.imgage} source={leftArrow} />
-  </TouchableOpacity>
-);
+const BackButton = ({ navigation }: Props) => {
+  const handleClick = () => navigation.goBack();
+  return (
+    <TouchableOpacity onPress={handleClick}>
+      <Image style={styles.imgage} source={leftArrow} />
+    </TouchableOpacity>
+  );
+};
 
 export default BackButton;
